214df00c0acdafbb6ee1e0522cb79e340cb9e8cb
HP <HP@DAYANA-CANCAN>
2026-07-03 19:46:07 -0500

    Agregar archivos de proyecto.

---
+++ Cancán_ProyectoIntegrador/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Cancán_ProyectoIntegrador.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cancán_ProyectoIntegrador.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cancán_ProyectoIntegrador/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cancán_ProyectoIntegrador.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
